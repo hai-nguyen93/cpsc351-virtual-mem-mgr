a8c45a3fa0dd15bcb397ff62851597e6a170a892
Hai Nguyen <54726078+hai-nguyen93@users.noreply.github.com>
2020-11-24 15:00:42 -0800

    added tlb

--- memmgr/memmgr.c
+++ memmgr/memmgr.c
@@ -17,6 +17,9 @@
 #define FRAME_SIZE  256
 
 char main_mem[65536];
+int tlb[16][2]; int current_tlb_entry=0;
+int page_table[256];
+int current_frame=0;
 FILE* fsto;
 
 //-------------------------------------------------------------------
@@ -31,19 +34,44 @@ void getpage_offset(unsigned x) {
          (page << 8) | getoffset(x), page * 256 + offset);
 }
 
-unsigned getframe_page_table(unsigned logic_add, unsigned page, int table[],
-          int *current_frame, int *page_fault_count){
-  // if page table hit
-  if (table[page] != -1) { return table[page];}
+int tlb_contains(unsigned x){
+  for (int i=0; i<16; i++){
+    if (tlb[i][0] == x) { return i; }
+    return -1;
+  }
+}
 
-  // page table miss
-  table[page] = (*current_frame);
-  (*current_frame)++;
+void update_tlb(unsigned page){
+  tlb[current_tlb_entry][0] = page;
+  tlb[current_tlb_entry][1] = page_table[page];
+  current_tlb_entry = (current_tlb_entry+1) % 16; // round-robin
+}
+
+unsigned getframe(unsigned logic_add, unsigned page,
+         int *page_fault_count, int *tlb_hit_count){
+  // tlb hit         
+  int tlb_index = tlb_contains(page);
+  if (tlb_index != -1) {
+    (*tlb_hit_count)++;
+    return tlb[tlb_index][1];
+  }
+
+  // tlb miss
+  // if page table hit
+  if (page_table[page] != -1) { 
+    update_tlb(page);
+    return page_table[page];
+  }
+
+  // page table miss -> page fault
+  page_table[page] = current_frame;
+  current_frame = (current_frame+1) % 256;
   (*page_fault_count)++;
   int offset = (logic_add / FRAME_SIZE) * FRAME_SIZE;
   fseek(fsto, offset, 0);
-  fread(&main_mem[table[page]*FRAME_SIZE],sizeof(char),256,fsto);
-  return table[page];
+  fread(&main_mem[page_table[page]*FRAME_SIZE],sizeof(char),256,fsto);
+  update_tlb(page);
+  return page_table[page];
 }
 
 int main(int argc, const char* argv[]) {
@@ -58,20 +86,17 @@ int main(int argc, const char* argv[]) {
   unsigned   logic_add;                  // read from file address.txt
   unsigned   virt_add, phys_add, value;  // read from file correct.txt
 
-  printf("ONLY READ FIRST 20 entries -- TODO: change to read all entries\n\n");
-
-  // not quite correct -- should search page table before creating a new entry
-      //   e.g., address # 25 from addresses.txt will fail the assertion
-      // TODO:  add page table code
-      // TODO:  add TLB code
+  int pfc[5]; // page fault count
+  int tlbh[5]; // tlb hit count
+  int count[5]; // access count
 
   fsto = fopen("BACKING_STORE.bin", "rb");   
   if (fsto == NULL) { fprintf(stderr, "Could not open file: 'BACKING_STORE.bin'\n");  exit(FILE_ERROR);  }
-
-  unsigned current_frame=0;
-  int page_table[256];
+  
+  // Initialize page table, tlb
   for (int i=0; i<256; ++i) { page_table[i]=-1; }
-  int tlb[16];
+  for (int i=0; i<16; ++i) { tlb[i][0]=-1; }
+  
   int access_count=0, page_fault_count=0, tlb_hit_count=0;
 
   while (fscanf(fadd, "%d", &logic_add) != EOF) {
@@ -83,19 +108,18 @@ int main(int argc, const char* argv[]) {
     // fscanf(fadd, "%d", &logic_add);  // read from file address.txt
     page   = getpage(  logic_add);
     offset = getoffset(logic_add);
-    frame = getframe_page_table(logic_add, page, page_table, &current_frame, &page_fault_count);
+    frame = getframe(logic_add, page, &page_fault_count, &tlb_hit_count);
 
     physical_add = frame * FRAME_SIZE + offset;
     int val = (int)(main_mem[physical_add]);
     fseek(fsto, logic_add, 0);
-    // char x = (fgetc(fsto));
-    
-    // for(int i=0; i<256; ++i){
-    //   printf("%d ", main_mem[frame * FRAME_SIZE +i]);
-    //   if (i%5==0) printf("\n");
-    // }
 
-    // todo: read BINARY_STORE and confirm value matches read value from correct.txt
+    // update tlb hit count and page fault count every 200 accesses
+    if (access_count > 0 && access_count%200==0){
+      tlbh[(access_count/200)-1] = tlb_hit_count;
+      pfc[(access_count/200)-1] = page_fault_count;
+      count[(access_count/200)-1] = access_count;
+    }
     
     printf("logical: %5u (page: %3u, offset: %3u) ---> physical: %5u -> value: %d-- passed\n", 
             logic_add, page, offset, physical_add, val);
@@ -105,20 +129,28 @@ int main(int argc, const char* argv[]) {
     assert(physical_add == phys_add);
     assert(value == val);
   }
+  tlbh[4] = tlb_hit_count;
+  pfc[4] = page_fault_count;
+  count[4] = access_count;
   fclose(fcorr);
   fclose(fadd);
   fclose(fsto);
   
-  printf("ONLY READ FIRST 20 entries -- TODO: change to read all entries\n\n");
-  
   printf("ALL logical ---> physical assertions PASSED!\n");
-  printf("!!! This doesn't work passed entry 24 in correct.txt, because of a duplicate page table entry\n");
-  printf("--- you have to implement the PTE and TLB part of this code\n");
-
-//  printf("NOT CORRECT -- ONLY READ FIRST 20 ENTRIES... TODO: MAKE IT READ ALL ENTRIES\n");
+  printf("ALL read memory value assertions PASSED!\n");
   
-  printf("page fault count: %d-------total count:%d\n", page_fault_count, access_count);
-  printf("page fault rate: %.3f\n", 1.0f*page_fault_count/access_count);
+  printf("\nStatistics:\n");
+  printf("access count\ttlb hit count\tpage fault count\ttlb hit rate\tpage fault rate\n");
+  for (int i=0; i<5; ++i){
+    printf("%d\t\t%d\t\t%d\t\t\t%.4f\t\t%.4f\n", count[i], tlbh[i], pfc[i],
+           1.0f*tlbh[i]/count[i], 1.0f*pfc[i]/count[i]);
+  }
+
   printf("\n\t\t...done.\n");
+
+  // for (int i=0; i<16; ++i){
+  //   printf("%d------%d\n", tlb[i][0], tlb[i][1]);
+  // }
+
   return 0;
 }
